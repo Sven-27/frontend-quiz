00bc6741904282f8d875a9d217063b71ee747876
Sven-27 <github.geometric664@passinbox.com>
2026-01-04 16:58:06 +0100

    deployed data.json on github and set cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    cors: false,
+    proxy: {
+      '/questions': {
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
