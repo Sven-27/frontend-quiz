f866dbf79725b1eb784b9f8264b9d6a197b9e3e3
Sven-27 <github.geometric664@passinbox.com>
2026-01-08 21:37:30 +0100

    added: fixed width to background image screen desktop and created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/header/Header";
+
 function App() {
 
   return (
-     <div className="container w-screen h-screen 2xl:max-h-[960px] overflow-hidden mx-auto p-4 bg-grey-50 flex items-center justify-center relative z-[-1]">
-htrhtrfg
+     <div className="container w-screen h-screen 2xl:max-h-[960px] overflow-hidden mx-auto p-4 bg-grey-50 flex  justify-center relative z-[-1]">
+        <Header />
     </div>
   )
 }
