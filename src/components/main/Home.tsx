a448259b73ababa300115c95956be40db0d06e79
Sven-27 <github.geometric664@passinbox.com>
2026-01-11 15:46:34 +0100

    stsarted home page

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -1,6 +1,12 @@
 const Home = () => {
   return (
-    <div className="h-full z-10">Home</div>
+    <main className="h-full z-10 w-full flex flex-col sm:flex-row items-start justify-center">
+      <section className="w-full h-full">
+        <h1 className="text-4xl mb-4 text-gray-800 dark:text-gray-200">Welcome to the <span className="font-bold">Frontend Quiz!</span></h1>
+        <p className="text-lg text-gray-600 dark:text-gray-400">Pick a subject to get started.</p>
+      </section>
+
+    </main>
   )
 }
 
