6600fed200fd9b7361ec32737d9246aa01ff74c0
Sven-27 <github.geometric664@passinbox.com>
2026-02-07 10:25:15 +0100

    added: seklected answer color

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -7,7 +7,7 @@ const ProgressBar = ({ progress }: ProgressBarProps) => {
   // Make sure our value stays between 0 and 100.
   const _progress = Math.min(Math.max(0, progress), 100);
   return (
-    <div className="flex flex-col items-center justify-center xl:pr-24">
+    <div className="flex flex-col items-center justify-center xl:pr-24 lg:mb-44">
       <div className="w-full bg-white dark:bg-blue-850 h-[1rem] rounded-xl p-1 flex items-center">
         <div
           className="bg-purple-600 h-full transition-all duration-250 rounded-xl"
--- src/components/main/Quiz.tsx
+++ src/components/main/Quiz.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import ProgressBar from "../ProgressBar"
 import { useQuestionsStore } from "../../zustand/questionsStore";
 import { useCountStore } from "../../zustand/countStore";
@@ -9,6 +9,7 @@ const Quiz = () => {
   const location = useLocation();
   const { questions, fetchQuestions }: any = useQuestionsStore();
   const { count, increment }: any = useCountStore();
+  const [selected, setSelected] = useState<number | null>(null);
 
 	useEffect(() => {
 		fetchQuestions();
@@ -16,10 +17,11 @@ const Quiz = () => {
 
   return (
     <div className="h-full w-full flex flex-col lg:flex-row z-10 gap-9">
-      <section className="w-full lg:grow-5">
+      <section className="w-full lg:grow-5 flex flex-col lg:justify-between">
+        <div>
         <span className="text-grey-500 italic text-[clamp(14px,2vw,20px)]">Question { count } of 10</span>
         <h2 
-          className="text-[clamp(20px,4vw,36px)] leading-[1.2] lg:max-w-[20ch] font-medium my-4 text-blue-900 font-light dark:text-gray-200 lg:mb-27"
+          className="text-[clamp(20px,4vw,36px)] leading-[1.2] lg:max-w-[20ch] font-medium my-4 text-blue-900 font-light dark:text-gray-200"
         >
           {
             questions.filter((topic: ResponseAPI) => `/quiz/${topic.title}` === location.pathname).map((topic: ResponseAPI) => 
@@ -27,15 +29,21 @@ const Quiz = () => {
             )
           }
         </h2>
+        </div>
         <ProgressBar progress={ count * 10} progressText="80%" />
       </section>
       <section className="w-full lg:grow-5 flex flex-col gap-4">
         {
           questions.filter((topic: ResponseAPI) => `/quiz/${topic.title}` === location.pathname).map((topic: ResponseAPI) => 
             Object.entries(topic.questions[count - 1].options).map(([key, option], index) => (
-              <button key={index} className="w-full flex items-center px-4 py-4 bg-white dark:bg-blue-850 text-blue-900 text-[clamp(16px,2vw,24px)] dark:text-white font-medium rounded-xl lg:rounded-3xl focus:ring-2 focus:ring-purple-600 hover:ring-2 hover:ring-purple-600 shadow-xl transition text-left">
-                <p className="size-[clamp(40px,4vw,56px)] grid place-items-center bg-grey-50 rounded-md mr-5 dark:text-blue-900">{`${key}`}</p>
-                <span>{`${option}`}</span>
+              console.log("Current selected state:", selected, "index", index),
+              <button 
+                key={index} 
+                onClick={() => setSelected(index)}
+                className={`option-btn w-full flex items-center px-4 py-4 bg-white dark:bg-blue-850 text-blue-900 text-[clamp(16px,2vw,24px)] dark:text-white font-medium rounded-xl lg:rounded-3xl  hover:ring-2 hover:ring-purple-600 shadow-xl transition text-left ${selected === index ? "ring-2 ring-purple-600" : ""}`}
+              >
+                <p className={`size-[clamp(40px,4vw,56px)] grid place-items-center bg-grey-50 rounded-md mr-5 dark:text-blue-900 ${selected === index ? "bg-purple-600 text-white dark:text-white" : ""}`}>{`${key}`}</p>
+                <span className="w-full">{`${option}`}</span>
               </button>
             ))
           )
@@ -43,7 +51,7 @@ const Quiz = () => {
         <button 
           type="button" 
           className="mt-3 w-full py-4 bg-purple-600 hover:bg-purple-400 text-white font-regular rounded-xl lg:rounded-3xl shadow-xl transition"
-          onClick={() => increment()}
+          onClick={() => count < 10 && increment()}
         >
           <span className="text-[clamp(16px,2vw,24px)]">Submit Answer</span>
         </button>
